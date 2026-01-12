52306232362ebaba1e6ffd19d072174aa20254b8
abhishek ies <abhisheksharma16250@gmail.com>
2026-01-12 11:11:22 +0530

    Fix vercel routing + cors permanently

--- api/index.js
+++ api/index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cors({
     origin: "https://ultimateqr-seven.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
 
 app.use(express.json());
